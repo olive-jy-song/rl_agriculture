54131b089a2d2cb63943bd6410445113f2c27b21
Jiayuan Song <js10417@nyu.edu>
2024-04-27 20:38:11 -0400

    modify steps

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ python train.py \
     --model_type DDPG \
     --config_name nebraska_maize_base \
     --save_dir '' \
-    --train_steps 10 \
+    --train_steps 250000 \
     --fig_dir ./nebraska_ppo \
     --plot_train_curve \
     --evaluate 
